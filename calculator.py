2595ea2a7f5fac53c676d994e9dafc79692b4ce8
Samiul Islam <44663019+samiul16@users.noreply.github.com>
2026-09-22 18:03:20 +0600

    python practice

--- calculator.py
+++ calculator.py
@@ -1,6 +1,12 @@
+from math import sqrt, ceil, floor
+
 def add(a, b):
     return a + b
 
 
 def subtract(a, b):
     return a - b
+
+# runs only when this file is executed directly
+if __name__ == "__main__":
+    add(1,3)
