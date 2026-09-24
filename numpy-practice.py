e37a2582ddb861829baab0676cffc13b25121e7d
Samiul Islam <44663019+samiul16@users.noreply.github.com>
2026-09-24 15:54:14 +0600

    numpy practice

--- numpy-practice.py
+++ numpy-practice.py
@@ -1,1 +1,11 @@
 import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+print(type(arr))
+print(arr.dtype)
+sum = np.sum(arr)
+print(sum)
+mean = np.mean(arr)
+print(mean)
+print(np.std(arr))
