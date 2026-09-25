c7ba7975f7042ca6cdfd9a2656515299b106290f
Samiul Islam <44663019+samiul16@users.noreply.github.com>
2026-09-25 15:28:19 +0600

    Matrix opertations with numpy

--- numpy-practice.py
+++ numpy-practice.py
@@ -14,3 +14,22 @@ matrix = np.array([[1, 2], [3, 4]])
 print(matrix)
 print(matrix.shape)
 print(matrix.dtype)
+
+
+
+matrix = np.array([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7,8, 9]
+])
+
+index1 = matrix[1,2]
+
+# select an index
+print(index1) 
+# select a row
+print(matrix[1])
+# select a column
+print(matrix[:, 1])
+
+
